2edb3d90e416dd647c6bcdc320a7cfb8f4b7481b
Eren Celik <erencelik2426@gmail.com>
2025-06-11 09:29:49 +0300

    Add home view returning centered welcome message

--- films/views.py
+++ films/views.py
@@ -1,7 +1,10 @@
+from http.client import HTTPResponse
+
 from django.shortcuts import render
 
 from django.contrib.auth.hashers import make_password
 from .models import Member
+from django.http import HttpResponse
 
 
 def create_member(info):
@@ -11,7 +14,8 @@ def create_member(info):
         email = info.POST['email']
         password = info.POST['password']
 
-    hashed_password = make_password(password)  # Bu düz şifre, Django’nun güvenli make_password() fonksiyonu ile hashlenir.
+    hashed_password = make_password(
+        password)  # Bu düz şifre, Django’nun güvenli make_password() fonksiyonu ile hashlenir.
 
     member = Member.objects.create(
         username=username,
@@ -19,4 +23,34 @@ def create_member(info):
         email=email,
         password=password
     )
-    member.save() # olusturulan nesne veri tabanına eklendi.
+    member.save()  # olusturulan nesne veri tabanına eklendi.
+
+
+def home(request):
+    return HttpResponse("""
+    <html>
+    <body>
+    <div style="display:flex; justify-content:center; align-items:center;" >
+    
+    <h1>Welcome home page!</h1>
+    </div>
+    
+    
+    
+    </body>
+    
+    
+    
+    
+    
+    </html>
+    
+    """)
+
+
+
+
+
+
+
+    # herhangi bir giriş kaydı basarılı ise bu mesaj gosterılecek.
