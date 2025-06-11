912d2e4285fcf8e1679dde2c5bbb1ddb96b8f3e5
Eren Celik <erencelik2426@gmail.com>
2025-06-11 09:28:12 +0300

    Register models to Django admin panel

---
+++ films/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Genre, Member, Film, Actor, Rating, Review, Director, Watchlist, MovieImage
+
+# modellerimizin admin panelinde gözükmesi için Django' ya kaydettim.
+admin.site.register(Genre)
+admin.site.register(Member)
+admin.site.register(Film)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(Review)
+admin.site.register(Director)
+admin.site.register(Watchlist)
+admin.site.register(MovieImage)
