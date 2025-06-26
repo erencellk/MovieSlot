9671f27d80d99669e37b2df86419006c836673eb
Eren Celik <erencelik2426@gmail.com>
2025-06-26 18:13:24 +0300

    manage admin panel

--- films/admin.py
+++ films/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Member, Film, Actor,  Review, Director, MovieImage, FilmMoreInfo, FilmComment,OscarAward , Login ,Register
+from .models import Member, Film, Actor,  Review, Director, MovieImage, FilmMoreInfo, FilmComment,OscarAward , Login ,Register,oneCıkanFilmler
 
 # FilmComment için özel admin
 class FilmCommentAdmin(admin.ModelAdmin):
@@ -18,4 +18,5 @@ admin.site.register(FilmMoreInfo)
 admin.site.register(OscarAward)
 admin.site.register(Login)
 admin.site.register(Register)
+admin.site.register(oneCıkanFilmler)
 admin.site.register(FilmComment, FilmCommentAdmin)  # özel admin sınıfıyla kaydettik
