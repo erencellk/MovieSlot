182de942f088c0e39bdbabef9c0578c2b19839f9
Eren Celik <erencelik2426@gmail.com>
2025-06-30 17:24:46 +0300

    manage js,css file

--- films/static/films/eren.js
+++ films/static/films/eren.js
@@ -13,3 +13,23 @@ document.addEventListener('DOMContentLoaded',()=>{
             });
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+      const savedEmail = localStorage.getItem('rememberedEmail');
+      if (savedEmail) {
+          document.getElementById('email').value = savedEmail;
+          document.querySelector('input[name="remember_me"]').checked = true;
+      }
+  });
+
+ document.getElementById('form').addEventListener('submit', function() {
+      const remember = document.querySelector('input[name="remember_me"]').checked;
+      const email = document.getElementById('email').value;
+
+      if (remember) {
+          localStorage.setItem('rememberedEmail', email);
+      } else {
+          localStorage.removeItem('rememberedEmail');
+      }
+  });
+
+
